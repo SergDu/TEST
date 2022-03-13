c143553ca1ff541391d837dbb342ada65b069656
SergDu <vip09023@yandex.ru>
2022-03-13 15:58:09 +0300

    repair_main.c [testing]

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
 {
 setlocale(LC_ALL, "Rus");
 int n = 0;
-while (n++ < 4)			/* строка 7 */
+while (n++ < 3)			/* строка 7 */
 printf("n равно %d\n", n); 	/* строка 8 */
 printf("Это все, что делает данная программа. \n");
 return 0;
